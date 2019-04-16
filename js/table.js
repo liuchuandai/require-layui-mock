158c3bce474985d5f91cb35de67fef3fcb517b4a
Author Name <Author Email>
2019-04-16 16:48:07 +0800

    almost all right

--- js/table.js
+++ js/table.js
@@ -15,19 +15,19 @@ require(["../lib/main"], function () {
                 , page: true //开启分页
                 , cols: [[ //表头
                     {field: 'id', title: 'ID', sort: true}
-                    , {field: 'username', title: '用户名', width: '10%'}
-                    , {field: 'sex', title: '性别', width: '7%', sort: true}
-                    , {field: 'city', title: '城市', width: '10%'}
-                    , {field: 'sign', title: '签名', width: '10%'}
-                    , {field: 'experience', title: '积分', width: '10%', sort: true}
-                    , {field: 'score', title: '评分', width: '10%', sort: true}
-                    , {field: 'classify', title: '职业', width: '10%'}
-                    , {field: 'wealth', title: '财富', width: '10%', sort: true}
-                    , {field: 'switchs', title: '开关 操作', width: '13%', templet: "#switchTpl"}
+                    , {field: 'username', title: '用户名'}
+                    , {field: 'sex', title: '性别', sort: true}
+                    , {field: 'city', title: '城市'}
+                    , {field: 'sign', title: '签名'}
+                    , {field: 'experience', title: '积分', sort: true}
+                    , {field: 'score', title: '评分', sort: true}
+                    , {field: 'classify', title: '职业'}
+                    , {field: 'wealth', title: '财富', sort: true}
+                    , {field: 'switchs', title: '开关 操作', width: '12%', templet: "#switchTpl"}
                 ]]
             });
             form.on('switch(sexDemo)', function (obj) {
-                layer.msg(JSON.stringify("状态 " + this.name + "&nbsp;&nbsp;&nbsp;" + "id " + obj.value));
+                layer.msg(JSON.stringify("状态 " + this.name +  "id " + obj.value));
             });
             //    del
             table.on('tool(test)', function (obj) {
@@ -38,6 +38,9 @@ require(["../lib/main"], function () {
                     });
                 }
             });
+        });
+        $("#tiaozhuang").click(function(){
+            window.location.herf = "#"
         })
     });
-})
+});
